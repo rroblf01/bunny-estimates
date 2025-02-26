68209be337fece2c9963d04a1abca7e656644374
Ricardo Robles <ricardo.r.f@hotmail.com>
2025-02-26 14:28:03 +0100

    Integrate eslint and prettier

--- estimates-snout/nuxt.config.ts
+++ estimates-snout/nuxt.config.ts
@@ -2,9 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   runtimeConfig: {
     public: {
       backendUrl: process.env.BACKEND_URL || 'localhost:8000'
     }
-  }
+  },
+
+  modules: ['@nuxt/eslint']
 })
