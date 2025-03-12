74bdcb4de04a8029d822f39309c645485478cde2
Ricardo Robles <ricardo.r.f@hotmail.com>
2025-03-12 11:04:08 +0100

    Add home button

--- estimates-snout/layouts/default.vue
+++ estimates-snout/layouts/default.vue
@@ -1,18 +1,34 @@
 <template>
-    <div class="layout">
-      <slot />
-    </div>
-  </template>
-  
-  <style>
-  html, body, .layout {
-    height: 100%;
-    margin: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #f5f5f5;
-    font-family: Arial, sans-serif;
-  }
-  
-  </style>
+  <div class="layout">
+    <nav class="navbar">
+      <a href="/">Inicio</a>
+    </nav>
+    <slot />
+  </div>
+</template>
+
+<style>
+html,
+body,
+.layout {
+  height: 100%;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f5f5f5;
+  font-family: Arial, sans-serif;
+}
+
+.navbar {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+}
+
+.navbar a {
+  text-decoration: none;
+  color: #000;
+  font-weight: bold;
+}
+</style>
